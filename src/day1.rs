9ef95217b66a909549c5c3a0dc8f0b32600c7f00
F-Anikulapo <fanikulapo@proton.me>
2022-11-08 20:39:55 +0100

    Fixed days handling. Real end of day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,27 @@
 /// How many measurements are larger than the previous measurement?
 use crate::common::read_input;
 
-pub fn solve() -> String {
+pub fn part1() {
     let values: Vec<u32> = read_input(1);
 
-    (1..values.len())
+    let r = (1..values.len())
         .map(|i| u32::from(values[i - 1] < values[i]))
         .sum::<u32>()
-        .to_string()
+        .to_string();
+
+    println!("Day 1 - Part 1: {}", r);
+}
+
+/// Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+pub fn part2() {
+    let values: Vec<u32> = read_input(1);
+
+    let r = (0..(values.len() - 3))
+        .map(|i| {
+            u32::from(values[i..i + 3].iter().sum::<u32>() < values[i + 1..i + 4].iter().sum())
+        })
+        .sum::<u32>()
+        .to_string();
+
+    println!("Day 1 - Part 2: {}", r);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::{env, process};
 
 mod common;
 mod day1;
+mod day2;
 
 fn main() {
-    let challenges = vec![day1::solve];
+    let challenges = [day1::part1, day1::part2];
 
     let args: Vec<String> = env::args().collect();
     if args.is_empty() {
@@ -12,13 +13,13 @@ fn main() {
         process::exit(exitcode::OSERR);
     } else if args.len() == 1 {
         println!("Running all the days:");
-        for (i, challenge) in challenges.iter().enumerate() {
-            println!("Day {}: {}", i, challenge());
-        }
+        challenges.iter().for_each(|x| x());
     } else if args.len() == 2 {
         if let Ok(day) = args[1].parse::<usize>() {
             println!("Running day {}", day);
-            println!("Result: {}", challenges[day - 1]());
+
+            challenges[day * 2 - 2]();
+            challenges[day * 2 - 1]();
         } else {
             eprintln!("Error: '{}' is not a valid day number.", args[1]);
             process::exit(exitcode::USAGE);
