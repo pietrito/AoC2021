e8a964c31a524472feaa53629bfd88ebae2e42b9
F-Anikulapo <fanikulapo@proton.me>
2022-11-17 13:08:41 +0100

    End of Day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,11 +1,13 @@
-use std::cmp::{max, min};
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+};
 
 /// Consider only horizontal and vertical lines. At how many points do at least two lines overlap?
 pub fn part1(input: &str) -> String {
-    let input_lines = input.lines();
     let mut lines: Vec<((i32, i32), (i32, i32))> = Vec::new();
 
-    for line in input_lines {
+    for line in input.trim().lines() {
         let (left, right) = if let Some((left, right)) = line.split_once(" -> ") {
             (left, right)
         } else {
@@ -52,9 +54,11 @@ pub fn part1(input: &str) -> String {
     let mut cells = vec![0; (max_x * max_y) as usize];
 
     for ((x1, y1), (x2, y2)) in lines {
+        // Only consider vertical and horizontal lines
         if x1 != x2 && y1 != y2 {
             continue;
         }
+        // for each point of the lines, increase their cell value
         for i in min(x1, x2)..=max(x1, x2) {
             for j in min(y1, y2)..=max(y1, y2) {
                 cells[(j as usize) * (max_x as usize) + (i as usize)] += 1;
@@ -62,20 +66,73 @@ pub fn part1(input: &str) -> String {
         }
     }
 
+    // Return the count of cells that contains more than one
     cells.iter().filter(|n| n > &&1).count().to_string()
 }
 
-pub fn part2(_: &str) -> String {
-    "".to_string()
+pub fn part2(input: &str) -> String {
+    let mut point_map: HashMap<(i32, i32), usize> = HashMap::new();
+    for line in input.trim().lines() {
+        let mut parts = line.split(" -> ");
+        let left: Vec<i32> = parts
+            .next()
+            .unwrap()
+            .split(',')
+            .map(|p| p.parse().unwrap())
+            .collect();
+        let right: Vec<i32> = parts
+            .next()
+            .unwrap()
+            .split(',')
+            .map(|p| p.parse().unwrap())
+            .collect();
+
+        let x1 = left[0];
+        let y1 = left[1];
+        let x2 = right[0];
+        let y2 = right[1];
+
+        // Horizontal line
+        if x1 == x2 {
+            for y in min(y1, y2)..=max(y1, y2) {
+                *point_map.entry((x1, y)).or_default() += 1;
+            }
+        }
+        // Vertical line
+        else if y1 == y2 {
+            for x in min(x1, x2)..=max(x1, x2) {
+                *point_map.entry((x, y1)).or_default() += 1;
+            }
+        }
+        // Diagonal line
+        else {
+            let dx = if x2 - x1 > 0 { 1 } else { -1 };
+            let dy = if y2 - y1 > 0 { 1 } else { -1 };
+
+            let mut x = x1;
+            let mut y = y1;
+            // First point
+            *point_map.entry((x, y)).or_default() += 1;
+            // Go through each point until reaching the endpoint of the line
+            while x != x2 {
+                x += dx;
+                y += dy;
+
+                *point_map.entry((x, y)).or_default() += 1;
+            }
+            // We should arrive at the (x2, y2) point
+            assert_eq!(y, y2);
+        }
+    }
+
+    point_map.values().filter(|p| **p > 1).count().to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part1() {
-        let input = r#"
+    const EXAMPLE_INPUT: &str = r#"
 0,9 -> 5,9
 8,0 -> 0,8
 9,4 -> 3,4
@@ -86,28 +143,14 @@ mod tests {
 3,4 -> 1,4
 0,0 -> 8,8
 5,5 -> 8,2
-            "#
-        .trim();
-
-        assert_eq!(part1(input), "5".to_string());
+            "#;
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(EXAMPLE_INPUT), "5".to_string());
     }
 
     #[test]
     fn test_part2() {
-        let input = r#"
-0,9 -> 5,9
-8,0 -> 0,8
-9,4 -> 3,4
-2,2 -> 2,1
-7,0 -> 7,4
-6,4 -> 2,0
-0,9 -> 2,9
-3,4 -> 1,4
-0,0 -> 8,8
-5,5 -> 8,2
-            "#
-        .trim();
-
-        assert_eq!(part2(input), "12".to_string());
+        assert_eq!(part2(EXAMPLE_INPUT), "12".to_string());
     }
 }
